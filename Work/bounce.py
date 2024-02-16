0922089e8ae68735f43654850942294102e22028
sunilnahata <sunilnahata4u@gmail.com>
2024-02-16 16:25:48 +0100

    work

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+# Height for 10 bounces
+
+height = 100 # in meters
+num_bounce = 1
+
+while num_bounce <= 10:
+    height = height * 3 / 5
+    print(f"Bounce # {num_bounce}, Height: {round(height, 4)}m")
+    num_bounce += 1
+    
