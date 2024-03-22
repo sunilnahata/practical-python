e7d344fe5d0678685e17eee2924d1e131fbcfc25
sunilnahata <sunilnahata4u@gmail.com>
2024-03-22 14:46:22 +0100

    exercise 2.12 (adding $)

--- Work/report.py
+++ Work/report.py
@@ -20,16 +20,16 @@ def read_portfolio(filename):
     return portfolio
 
 def read_prices(filename):
-    prices = {}
+    price = {}
     with open(filename) as f:
         rows = csv.reader(f)
         for row in rows:
             try:
-                prices[row[0]] = float(row[1])
+                price[row[0]] = float(row[1])
             except IndexError:
                 pass
     
-    return prices
+    return price
 
 def make_report(price, portfolio):
     rows = []
@@ -45,16 +45,23 @@ def make_report(price, portfolio):
 portfolio = read_portfolio('Data/portfolio.csv')
 price = read_prices('Data/prices.csv')
 
-cost = 0
-for share in portfolio:
-    cost += share["shares"] * share["price"]
-print(f"Total cost: {cost}")
 
-curr_val = 0
-for share in portfolio:
-    curr_val += share["shares"] * price[share["name"]]
+report = make_report(price, portfolio)
+header = ('Name', 'Shares', 'Price', 'Change')
+print("%10s %10s %10s %10s" %header)
+print(("-" * 10 + " ") * len(header))
+for name, shares, price, change in report:
+    print(f"{name:>10s} {shares:>10d} {f'${price:.2f}':>10} {change:>10.2f}")
 
-print(f"Current value: {curr_val}")
-print(f"Profit: {curr_val - cost}")
-
-
+#cost = 0
+#for share in portfolio:
+#    cost += share["shares"] * share["price"]
+#print(f"Total cost: {cost}")
+#
+#curr_val = 0
+#for share in portfolio:
+#    curr_val += share["shares"] * price[share["name"]]
+#
+#print(f"Current value: {curr_val}")
+#print(f"Profit: {curr_val - cost}")
+#
