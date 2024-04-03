ed38c10a52288b6e460d34bcfbfa8f365c11d099
sunilnahata <sunilnahata4u@gmail.com>
2024-04-03 20:46:12 +0200

    exercise 3.2

--- Work/report.py
+++ Work/report.py
@@ -52,25 +52,12 @@ def print_report(report):
     for name, shares, price, change in report:
         print(f"{name:>10s} {shares:>10d} {f'${price:.2f}':>10} {change:>10.2f}")
 
-##cost = 0
-#for share in portfolio:
-#    cost += share["shares"] * share["price"]
-#print(f"Total cost: {cost}")
-#
-#curr_val = 0
-#for share in portfolio:
-#    curr_val += share["shares"] * price[share["name"]]
-#
-#print(f"Current value: {curr_val}")
-#print(f"Profit: {curr_val - cost}")
-#
-def main():
-    portfolio = read_portfolio('Data/portfoliodate.csv')
+
+def portfolio_report(portfoliofile, pricefile):
+    portfolio = read_portfolio(portfoliofile)
     #portfolio = read_portfolio(input("Enter the filename: ")) 
-    prices = read_prices('Data/prices.csv')
+    prices = read_prices(pricefile)
     report = make_report(portfolio, prices)
     print_report(report)
 
-if __name__ == "__main__":
-    main()
-
+portfolio_report('Data/portfoliodate.csv', 'Data/prices.csv')
